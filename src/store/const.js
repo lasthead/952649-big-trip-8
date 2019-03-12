d3e5ab5560777bbaceaac2789d51b93d50823671
lasthead <war-bird@mail.ru>
2019-03-12 14:19:51 +0300

    Перенес списки фильтров в store

---
+++ src/store/const.js
@@ -0,0 +1,4 @@
+
+export const mainFilter = document.getElementsByClassName(`trip-filter`)[0];
+export const boardTrips = document.getElementsByClassName(`trip-day__items`)[0];
+
---
+++ src/store/tripsList.js
@@ -0,0 +1,23 @@
+export const trips =
+  [
+    {
+      name: `Taxi to Airport`,
+      icon: `🚗`,
+      timeBegin: `10:00`,
+      timeEnd: `11:00`,
+      price: 20,
+      currency: `€`,
+      offers: [
+        {
+          name: `Order UBER`,
+          price: 20,
+          currency: `€`,
+        },
+        {
+          name: `Upgrade to business`,
+          price: 20,
+          currency: `€`,
+        }
+      ],
+    },
+  ];
