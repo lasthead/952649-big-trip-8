1c8dd59de1a39543ede711c4311f130936efbb3b
lasthead <war-bird@mail.ru>
2019-03-14 11:16:15 +0300

    esLint bugfix

--- src/makeTrip.js
+++ src/makeTrip.js
@@ -1,6 +1,6 @@
 
 
-export default ({icon, title, timetable, duration, price, currency, offers}) => `
+export default ({icon, title, timetable, duration, price, currency}) => `
     <article class="trip-point">
       <i class="trip-icon">${ icon }</i>
       <h3 class="trip-point__title">${ title }</h3>
