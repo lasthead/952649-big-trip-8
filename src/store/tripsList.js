da6c0c8f7b12d32579363e2e2cda2505f84ee75a
lasthead <war-bird@mail.ru>
2019-03-20 23:37:59 +0300

    структура данных

--- src/store/tripsList.js
+++ src/store/tripsList.js
@@ -2,22 +2,55 @@ export const trips =
   [
     {
       title: `Taxi to Airport`,
-      icon: `🚗`,
+      icon: {
+        Taxi: `🚕`,
+        Bus: `🚌`,
+        Train: `🚂`,
+        Ship: `🛳️`,
+        Transport: `🚊`,
+        Drive: `🚗`,
+        Flight: `✈️`,
+        CheckIn: `🏨`,
+        Sightseeing: `🏛️`,
+        Restaurant: `🍴`
+      },
+      picture: `http://picsum.photos/300/150?r=${Math.random()}`,
       timetable: `10:00 - 11:00`,
       duration: `1h 30m`,
       price: 20,
       currency: `€`,
       offers: [
         {
-          name: `Order UBER`,
+          name: `Add luggage`,
           price: 20,
           currency: `€`,
         },
         {
-          name: `Upgrade to business`,
+          name: `Switch to comfort class`,
           price: 20,
           currency: `€`,
-        }
+        },
+        {
+          name: `Add meal`,
+          price: 20,
+          currency: `€`,
+        },
+        {
+          name: `Choose seats`,
+          price: 20,
+          currency: `€`,
+        },
       ],
+      about: new Set([
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+        `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+        `Fusce tristique felis at fermentum pharetra.`,
+        `Aliquam id orci ut lectus varius viverra.`,
+        `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+        `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+        `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+        `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+        `Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus.`,
+        `In rutrum ac purus sit amet tempus`]),
     },
   ];
